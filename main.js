0226243fb95dcf10ad9b96bf055da231dfa45254
Stuckinaboot <aspyn@seas.upenn.edu>
2017-07-23 06:11:42 -0400

    Implement upload and download files to server

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT_NUM = 8001;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({limit: '100 mb'}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const VIEWS = {
     main: "index.ejs"
@@ -13,6 +16,20 @@ app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     res.render(VIEWS.main);
+});
+
+app.post("/uploadipfs", (req, res) => {
+	let contents = req.body.file.contents;
+	let fileName = req.body.file.name;
+	res.send("Ok");
+});
+
+app.get("/downloadipfs", (req, res) => {
+	let hash = req.query.hashToDl;
+
+	//TODO: Send back a file
+	res.set("Content-Disposition", "attachment");
+	res.send("Ok");
 });
 
 app.listen(PORT_NUM, () => console.log("Distribute the Press --> Profit"));
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,21 @@
 $(function () {
 
+	// Utilities
+
+	function downloadFile(url, success) {
+	    var xhr = new XMLHttpRequest(); 
+	    xhr.open('GET', url, true); 
+	    xhr.responseType = "blob";
+	    xhr.onreadystatechange = function () { 
+	        if (xhr.readyState == 4) {
+	            if (success) success(xhr.response);
+	        }
+	    };
+	    xhr.send(null);
+	}
+
 	// Backbone
+
 	var Downloadable = Backbone.Model.extend({});
 	var Downloadables = Backbone.Collection.extend({
 		model: Downloadable
@@ -37,6 +52,8 @@ $(function () {
 	    }
 	];
 
+
+
 	var ClickableRow = Backgrid.Row.extend({
 	    events: {
 	        "click": "onClick"
@@ -45,11 +62,28 @@ $(function () {
 	        let model = this.model;
 	        console.log(model.attributes);
 
-		var tingleModal = new tingle.modal({
-		});
-	        let str = JSON.stringify(model.attributes);
-	        console.log(str);
-	        tingleModal.setContent(str);
+			var tingleModal = new tingle.modal({
+				footer: true,
+				closeMethods: ['overlay', 'button', 'escape']
+			});
+	        tingleModal.setContent("Download file " 
+	        	+ model.attributes.name + " (" + model.attributes.hash + ")");
+
+			// add a button
+			tingleModal.addFooterBtn('Download', 'tingle-btn tingle-btn--primary', function() {
+			    downloadFile("/downloadipfs?hashToDl=" + model.attributes.hash, 
+			    	(a) => {
+							saveAs(new Blob([a]), fileName);
+						}
+				);
+			});
+
+			// add another button
+			tingleModal.addFooterBtn('Cancel', 'tingle-btn tingle-btn--danger', function() {
+			    // here goes some logic
+			    tingleModal.close();
+			});
+
 	        tingleModal.open();
 	    }
 	});
@@ -66,24 +100,52 @@ $(function () {
 
 	// -- Upload button -- 
 
+
+
+	var fileName;
+	var fileContents;
 	// Read file data on upload
 	$("#in-file").change(function () {
 		var reader = new FileReader();
-		let file = this.files[0];
-    	reader.onload = function () {
-        	var buffer = this.result;
-      		// console.log(buffer);
-      		downloadables.push({
-      			fileName: "File name",
-      			dateAdded: "1",
-      			numDownloads: "2",
-      			numUpVotes: "3",
-      			numDownVotes: "4",
-      			contents: buffer
-      		});
-      		view.render();
-        }
-        console.log(file);
-    	reader.readAsBinaryString(file);
+		if (this.files.length > 0) {
+			let file = this.files[0];
+	    	reader.onload = function () {
+	        	var buffer = this.result;
+	      		fileName = file.name;
+	      		fileContents = buffer;
+	      		$("#btn-file-submit").show();
+	      		downloadables.push({
+	      			name: "File name",
+	      			dateAdded: "1",
+	      			numDownloads: "2",
+	      			numUpVotes: "3",
+	      			numDownVotes: "4",
+	      			contents: buffer,
+	      			hash: "abcde"
+	      		});
+
+	        }
+	        console.log(file);
+	    	reader.readAsBinaryString(file);
+    	} else {
+    		fileName = undefined;
+    		fileContents = undefined;
+    		$("#btn-file-submit").hide();
+    	}
 	});
+
+	$("#btn-file-submit").click(() => {
+		console.log(fileName);
+		$.post("/uploadipfs", 
+			{
+				file: {
+					name: fileName,
+					contents: fileContents
+				}
+			}, (a) => {
+				console.log(a);
+			}
+		)
+	});
+
 });
