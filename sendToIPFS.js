842b8dc65ab084ba91b758de67ee63f9c49e278c
Stuckinaboot <aspyn@seas.upenn.edu>
2017-07-23 07:29:13 -0400

    Add promises

--- sendToIPFS.js
+++ sendToIPFS.js
@@ -7,24 +7,28 @@ var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'})
 function upload(name, data) {
   const file = [ new Buffer(JSON.stringify({name: name, data: data})) ];
 
-  ipfs.files.add(file, function(err, files) {
-    if (err) throw err;
-    console.log(files[0].hash);
-    return files[0].hash;
-  })
+  return new Promise((resolve, reject) => {
+    ipfs.files.add(file, function(err, files) {
+      if (err) throw err;
+      console.log(files[0].hash);
+      resolve(files[0].hash);
+    })
+  });
 }
 //upload('sup.txt', "hesdfllo");
 
 function download(hash) {
   var content;
-  ipfs.files.cat(hash, function(err, stream) {
-    if (err) throw err;
-    stream.pipe(bl((err, data) => {
-      content = JSON.parse(data.toString());
-      console.log(content);
-    }))
+  return new Promise((resolve, reject) => {
+    ipfs.files.cat(hash, function(err, stream) {
+      if (err) throw err;
+      stream.pipe(bl((err, data) => {
+        content = JSON.parse(data.toString());
+        console.log(content);
+        resolve(content);
+      }))
+    });
   });
-  return content;
 }
 
 module.exports = {
