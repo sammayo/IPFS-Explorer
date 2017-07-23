f421e142bde278a9f37528ebef1d2318340073f1
Stuckinaboot <aspyn@seas.upenn.edu>
2017-07-23 03:43:07 -0400

    Add missing dependencies. Add basic backbone support

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
 $(function () {
+
+	// Backbone
 	var Downloadable = Backbone.Model.extend({});
 	var Downloadables = Backbone.Collection.extend({
 		model: Downloadable
@@ -6,20 +8,82 @@ $(function () {
 
 	var downloadables = new Downloadables();
 
-	var DownloadablesView = Backbone.View.extend({
-		initialize: function () {
-			console.log("INIT VIEW");
-		}
+	var columns = [
+	    {
+	        name: "fileName",
+	        label: "File Name",
+	        cell: "string",
+	        editable: false
+	    }, {
+	        name: "dateAdded",
+	        label: "Date Added",
+	        cell: "string",
+	        editable: false
+	    }, {
+	        name: "numDownloads",
+	        label: "Downloads",
+	        cell: "string",
+	        editable: false
+	    }, {
+	        name: "numUpVotes",
+	        label: "Up Votes",
+	        cell: "string",
+	        editable: false
+	    }, {
+	        name: "numDownVotes",
+	        label: "Down Votes",
+	        cell: "string",
+	        editable: false
+	    }
+	];
+
+	var ClickableRow = Backgrid.Row.extend({
+	    events: {
+	        "click": "onClick"
+	    },
+	    onClick: function () {
+	        let model = this.model;
+	        console.log(model.attributes);
+
+		var tingleModal = new tingle.modal({
+		});
+	        let str = JSON.stringify(model.attributes);
+	        console.log(str);
+	        tingleModal.setContent(str);
+	        tingleModal.open();
+	    }
 	});
 
-	var view = new DownloadablesView({
-		el: $("#view-downloadables"),
-		collection: downloadables,
-		render: function () {
-			console.log("HI");
-			return this;
-		}
+	// Initialize a new Grid instance
+	var grid = new Backgrid.Grid({
+	    columns: columns,
+	    collection: downloadables,
+	    row: ClickableRow
 	});
-	console.log(view);
-	view.render();
+
+	// Render the grid and attach the root to your HTML document
+	$("#view-downloadables").append(grid.render().el);
+
+	// -- Upload button -- 
+
+	// Read file data on upload
+	$("#in-file").change(function () {
+		var reader = new FileReader();
+		let file = this.files[0];
+    	reader.onload = function () {
+        	var buffer = this.result;
+      		// console.log(buffer);
+      		downloadables.push({
+      			fileName: "File name",
+      			dateAdded: "1",
+      			numDownloads: "2",
+      			numUpVotes: "3",
+      			numDownVotes: "4",
+      			contents: buffer
+      		});
+      		view.render();
+        }
+        console.log(file);
+    	reader.readAsBinaryString(file);
+	});
 });
